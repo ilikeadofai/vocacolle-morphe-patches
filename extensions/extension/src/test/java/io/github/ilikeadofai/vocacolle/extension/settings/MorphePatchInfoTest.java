c91bf5b1fcb8f74b9512610e76415ec96226b3e1
Matt Choi <mattchoi10828@gmail.com>
2026-07-23 21:27:35 +0900

    test: avoid hardcoded prerelease version

--- extensions/extension/src/test/java/io/github/ilikeadofai/vocacolle/extension/settings/MorphePatchInfoTest.java
+++ extensions/extension/src/test/java/io/github/ilikeadofai/vocacolle/extension/settings/MorphePatchInfoTest.java
@@ -2,13 +2,14 @@ package io.github.ilikeadofai.vocacolle.extension.settings;
 
 import static org.junit.Assert.assertEquals;
 
+import io.github.ilikeadofai.vocacolle.extension.BuildConfig;
 import org.junit.Test;
 
 public class MorphePatchInfoTest {
     @Test
     public void appVersionIncludesTheCurrentMorphePatchVersion() {
         assertEquals(
-                "7.40.0 · Morphe 1.1.0-dev.1",
+                "7.40.0 · Morphe " + BuildConfig.MORPHE_PATCH_VERSION,
                 MorphePatchInfo.formatAppVersion("7.40.0")
         );
     }
